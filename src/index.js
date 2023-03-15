d621fa7ede3fff5e8e395cbc2cab44a685f388aa
Abdul Raheman Anas <109565032+Anasmomin143@users.noreply.github.com>
2023-03-16 01:46:57 +0530

    deployed on GCP Firebase

--- src/index.js
+++ src/index.js
@@ -4,6 +4,33 @@ import './index.css';
 import App from './App';
 import 'animate.css';
 
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBkc5Mab1HnAheSEvu2hNw81UAH1B0Abu4",
+  authDomain: "indanikah.firebaseapp.com",
+  projectId: "indanikah",
+  storageBucket: "indanikah.appspot.com",
+  messagingSenderId: "484703610329",
+  appId: "1:484703610329:web:1386ded71b4b907ba5e5c8",
+  measurementId: "G-PFT9HSPCEF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+
+///
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
